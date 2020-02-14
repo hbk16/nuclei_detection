feda436c816b758cb8c92a5d2014590de1ede8b9
hbk16 <lonelyhbk@qq.com>
2020-02-14 23:22:52 +0800

    Add files via upload

---
+++ histomicstk/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+# import sub-packages to support nested calls
+from . import utils  # must be imported before other packages
+
+from . import segmentation  # must be imported before features
+
+from . import features
+from . import filters
+from . import preprocessing
+
+# list out things that are available for public use
+__all__ = (
+
+    # sub-packages
+    'features',
+    'filters',
+    'preprocessing',
+    'segmentation',
+    'utils',
+)
